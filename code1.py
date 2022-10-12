94c472752f083a408f5be025e8d55f641b9df213
SaxHornet <7604414+SaxHornet@users.noreply.github.com>
2022-10-12 12:03:47 +0200

    Update code1.py

--- code1.py
+++ code1.py
@@ -27,3 +27,16 @@ ZbetaZ = z @ betaZ
 residus = np.square(LA.norm(np.subtract(ZbetaZ,y)))
 
 print ("Résidus question 3 : \n\n",residus)
+
+
+#ipython question3.py 
+#Datas chargées ...
+
+#Le betaZ : 
+
+# [-5.05906229e-02 -1.95851023e+00 -2.93492412e-02  2.49883984e-02
+# -9.42582369e-01  4.79078658e-03 -8.77630817e-04  2.04204607e+00
+ # 1.68395142e-01  4.16453560e-01  3.65633380e-01]
+#Résidus question 3 : 
+
+ #2794.3435114292647
